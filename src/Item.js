675b1e39414012fabac65e690168df5117164e9e
kumata-san <kumatasan2019@gmail.com>
2020-01-29 23:35:09 +0900

    add styled-components, axios and Item.js

---
+++ src/Item.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Item = ({ id, text, deleteTodo }) => {
+    const [isDone, setIsDone] = useState(false)
+    const btnStatus = isDone ? '未完了に戻す' : '完了！'
+    const handleDelete = () => {
+        deleteTodo(id)
+    }
+    return (
+        <ItemWrapper>
+            <p>{`${id}: ${text}`}</p>
+            <button onClick={() => setIsDone(!isDone)}>
+                {btnStatus}
+            </button>
+            <button onClick={handleDelete}>削除</button>
+        </ItemWrapper>
+    )
+}
+
+const ItemWrapper = styled.li`
+    background-color: gray;
+`
+
+export default Item
